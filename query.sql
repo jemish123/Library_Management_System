5c7d152155650c7bff3b86df2384fc04a9b56f12
jemish123 <jemishgm103@gmail.com>
2025-07-01 22:31:18 +0530

    Library Management System to manage and track books .

---
+++ query.sql
@@ -0,0 +1,98 @@
+--  ---------------------------  Tasks for LMS  --------------------------
+-- 1. Create a new book with the following data:
+-- '978-1-60125-456-2', 'To Kill a Mockingbird', 'Calssic', 6.00, 'yes', 'Harper Lee', 'J.B. Lippincott & Co.'
+insert into
+	Books
+(isbn, book_title, category, rental_price, status, author, publisher)
+	values
+('978-1-60125-456-2', 'To Kill a Mockingbird', 'Calssic', 6.00, 'yes', 'Harper Lee', 'J.B. Lippincott & Co.');
+
+select * from Books;
+
+
+--2. Update an Existing Member's Address. (Pick any of your choice.)
+select * from Members;
+update Members set member_address='792 Oak St' where member_id='C103';
+
+
+-- 3. Delete a record from the issued status table : Objective (Delete record with issue_id = 'IS121' from the Issued_Status)
+select * from Issue_Status;
+delete from Issue_Status where issue_id='IS121';
+
+
+-- 4. List all books issued by employee with emp_id='E101'
+select * from issue_status;
+select * from Issue_Status where issued_emp_id='E101';
+
+
+--5. List all members who have issued more than 1 book.
+select 
+	issued_member_id, count(*) as issued_books_count 
+from issue_status 
+	group by issued_member_id
+	having count(*) > 1;
+
+
+-- 6. CTAS (Create Table As Select)
+-- Generate a new table which holds record of books and its respective issued_count
+Create Table book_issued_count as (
+	select 
+		b.isbn, b.book_title, count(i.issue_id) as Issue_Count
+	from
+		Books as b Left join Issue_Status as i
+	on
+		b.isbn=i.issued_book_isbn
+	group by b.isbn, b.book_title
+);
+select * from book_issued_count;
+
+
+-- 7. Fetch all books in a category 'History'
+select * from Books where category='History';
+
+
+-- 8. Find Total Rental Income By Category.
+select 
+	b.category as category, sum(b.rental_price) as total_revenue 
+from 
+	Issue_Status as i join Books as b 
+	on i.issued_book_isbn=b.isbn
+	group by category 
+	order by total_revenue desc;
+
+
+--9. List all members who registered in last 1080 days.
+select 
+	*
+from Members
+	where reg_date >= CURRENT_DATE - INTERVAL '1080 days';
+
+
+-- 10. List Employees with their branch manager's name and branch details
+select 
+	e1.emp_id,
+	e1.emp_name,
+	e1.job_position,
+	b.*,
+	e2.emp_name
+from 
+	Employee as e1 join Branch as b on e1.branch_id=b.branch_id
+	join Employee as e2 on b.manager_id=e2.emp_id;
+
+
+-- 11. Create a table which list all books which rental price is above 20.00
+create table Books_Rental_Threshold as (
+	select * from Books where rental_price > 4
+);
+select * from Books_Rental_Threshold;
+
+
+-- 12. Retrieve the list of books not yet returned
+select * from return_status;
+select 
+	*
+from 
+	issue_status as i join return_status as r
+	on i.issue_id=r.issued_id
+	where r.return_id IS NULL;
+	
---
+++ setup.sql
@@ -0,0 +1,82 @@
+-- Create a Database for LMS.
+CREATE DATABASE LMS_db;
+
+-- -------------------- CREATE TABLES ----------------------
+CREATE TABLE IF NOT EXISTS Branch (
+	branch_id varchar(10) PRIMARY KEY,
+	manager_id varchar(10),
+	branch_address varchar(50),
+	contact_no varchar(15)
+
+	foreign key (manager_id) references Employee(emp_id)
+);
+
+CREATE TABLE IF NOT EXISTS Employee (
+	emp_id varchar(10) PRIMARY KEY,
+	emp_name varchar(30),
+	job_position varchar(30),
+	salary numeric(10,2),
+	branch_id varchar(10),
+	
+	foreign key (branch_id) references Branch(branch_id)
+);
+
+CREATE TABLE IF NOT EXISTS Members (
+	member_id varchar(10) PRIMARY KEY,
+	member_name varchar(30),
+	member_address varchar(50),
+	reg_date date
+);
+
+CREATE TABLE IF NOT EXISTS Books (
+	isbn varchar(50) primary key,
+	book_title varchar(80),
+	category varchar(30),
+	rental_price numeric(10,2),
+	status varchar(10),
+	author varchar(30),
+	publisher varchar(30)
+);
+
+CREATE TABLE IF NOT EXISTS Issue_Status(
+	issue_id varchar(10) PRIMARY KEY,
+	issued_member_id varchar(30),
+	issued_book_name varchar(50),
+	issued_date date,
+	issued_book_isbn varchar(50),
+	issued_emp_id varchar(10),
+
+	foreign key (issued_member_id) references Members(member_id),
+	foreign key (issued_book_isbn) references Books(isbn),
+	foreign key (issued_emp_id) references Employee(emp_id)
+);
+
+CREATE TABLE IF NOT EXISTS Return_Status (
+	return_id varchar(10) PRIMARY KEY,
+	issue_id varchar(10),
+	return_book_name varchar(30),
+	return_date date,
+	return_book_isbn varchar(50),
+
+	foreign key (return_book_isbn) references Books(isbn)
+);
+
+
+-- -------------------- INSERT DATA into TABLES ----------------------
+COPY Branch (branch_id,	manager_id,	branch_address,	contact_no) FROM '/tmp/branch.csv' DELIMITER ',' CSV HEADER;
+select * from Branch;
+
+COPY Employee (emp_id,	emp_name, job_position,	salary, branch_id) FROM '/tmp/employees.csv' DELIMITER ',' CSV HEADER;
+select * from Employee;
+
+COPY Members (member_id, member_name, member_address, reg_date) FROM '/tmp/members.csv' DELIMITER ',' CSV HEADER;
+select * from Members;
+
+COPY Books (isbn, book_title, category,	rental_price, status, author, publisher) FROM '/tmp/books.csv' DELIMITER ',' CSV HEADER;
+select * from Books;
+
+COPY Issue_Status (issue_id, issued_member_id, issued_book_name, issued_date, issued_book_isbn, issued_emp_id) FROM '/tmp/issued_status.csv' DELIMITER ',' CSV HEADER;
+select * from Issue_Status;
+
+COPY Return_Status (return_id, issued_id, return_date) FROM '/tmp/return_status.csv' DELIMITER ',' CSV HEADER;
+select * from Return_Status;
